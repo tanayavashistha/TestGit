f8338b69ec92e361d9ad4b8439b50dce3f4ca556
Tanaya Vashistha <tanaya.vashistha@gmail.com>
2021-02-07 11:40:36 -0800

    Added Sum feature in Calc

---
+++ src/test/java/Calculator.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Sum sObj = new Sum();
+        System.out.println("Sum = " +sObj.doSum(1,2));
+
+    }
+}
---
+++ src/test/java/Sum.java
@@ -0,0 +1,5 @@
+public class Sum {
+    public int doSum(int a, int b){
+        return a+b;
+    }
+}
